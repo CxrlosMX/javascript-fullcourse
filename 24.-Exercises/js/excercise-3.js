2d7c6c2d2b138b9295da6a344fe33ff647255d5d
CxrlosMX <LuisCRendon131@gmail.com>
2021-11-07 16:30:04 -0600

    Exercise-3 complete

--- 24.-Exercises/js/excercise-3.js
+++ 24.-Exercises/js/excercise-3.js
@@ -1,4 +1,14 @@
 //3) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
-function repitWord(number,word) {
+function repitWord(number, word) {
+    if (typeof word == 'string') {
+        let text="";
+        for (let i = 0; i < number; i++) {
+            text+=word+"  "
+        }
+        return text;
+    }
+       return `please introduce a correct value(String)`;
     
 }
+
+console.log(repitWord(4,3));
