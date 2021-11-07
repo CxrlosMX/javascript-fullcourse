3bd4d13bf6fc1816db226a349c5c2b30c45b0f8f
CxrlosMX <LuisCRendon131@gmail.com>
2021-11-07 16:20:51 -0600

    Exercise-1 add typeof

---
+++ 24.-Exercises/js/excercise-3.js
@@ -0,0 +1,4 @@
+//3) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+function repitWord(number,word) {
+    
+}
--- 24.-Exercises/js/exercise-1.js
+++ 24.-Exercises/js/exercise-1.js
@@ -1,16 +1,16 @@
 // 1) Program a function that counts the number of characters in a text string, eg. myFunction ("Hello World") will return 10.
 
 function lenghtText(text) {
-    if (typeof Text) {
+    if (typeof text=='string') {
         let j = 0;
         for (let i = 0; i < text.length; i++) {
             j += 1;
         }
         return j;
     }
-    return 0;
+    return "You have to introduce only String caracters";
 }
-console.log(`The string has a length of ${lenghtText("My names is CxrlosMX")}`);
+console.log(`The string has a length of ${lenghtText("Hola")}`);
 
 
 
