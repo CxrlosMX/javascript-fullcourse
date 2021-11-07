fefd0e029cb98a46c97233e985f578996426a2c8
CxrlosMX <LuisCRendon131@gmail.com>
2021-11-07 16:52:59 -0600

    Exercise-1 fixed

--- 24.-Exercises/js/exercise-1.js
+++ 24.-Exercises/js/exercise-1.js
@@ -1,12 +1,8 @@
 // 1) Program a function that counts the number of characters in a text string, eg. myFunction ("Hello World") will return 10.
 
 function lenghtText(text) {
-    if (typeof text=='string') {
-        let j = 0;
-        for (let i = 0; i < text.length; i++) {
-            j += 1;
-        }
-        return j;
+    if (typeof text == 'string') {
+        return text.length;
     }
     return "You have to introduce only String caracters";
 }
@@ -18,5 +14,4 @@ console.log(`The string has a length of ${lenghtText("Hola")}`);
 
 /*
 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
-4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
 */
