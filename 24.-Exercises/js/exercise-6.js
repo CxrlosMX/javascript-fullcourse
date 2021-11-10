77d253a9bce9b00d58e02969723489b2c7fe91af
CxrlosMX <LuisCRendon131@gmail.com>
2021-11-10 09:20:42 -0600

    Exercise 11 resolved

--- 24.-Exercises/js/exercise-6.js
+++ 24.-Exercises/js/exercise-6.js
@@ -14,14 +14,28 @@ function randomN(max, min) {
 /*10) Program a function that receives a number and evaluates whether it is capicúa or not (which reads the same in one sense as in another), eg. myFunction (2002) will return true.*/
 const capicuaNumber = (number = undefined) => {
     if (number === undefined) return console.warn(`Please introduce a number`);
-    if(typeof number!==`number`)return console.error("Please introduce a number");
+    if (typeof number !== `number`) return console.error("Please introduce a number");
     number = number.toString();
     if (number.split(``).reverse().join(``) === number) {
         return console.info(true);
     }
     return console.info(false);
 }
-capicuaNumber(2002);
+//capicuaNumber(212.212);
 
-/*
-11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.*/
+/*11) Program a function that calculates the factorial of a number (The factorial of a positive integer n, is defined as the product of all positive integers from 1 to n), eg. myFunction (5) will return 120.*/
+const factorial = (number = undefined) => {
+    if (number === undefined || typeof number !== `number`) return console.warn(`Please introduce a number`);
+    if (number <= 0) return console.warn(`Please introduce a positive number`);
+
+    let result=1;
+    for (let i = 1; i <=number; i++) {
+    //result=result*i;
+    //This is the same but is a simplification
+    result*=i;        
+    }
+    return console.info(`The factorial of the number ${number} is: ${result}`);
+}
+factorial(5);
+
+
