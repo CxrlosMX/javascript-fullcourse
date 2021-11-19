a6c6858765bb33ba29818420b2e75cfaba2a439e
CxrlosMX <LuisCRendon131@gmail.com>
2021-11-19 09:35:28 -0600

    Exercise 24  resolved

--- 24.-Exercises/js/exercise-11.js
+++ 24.-Exercises/js/exercise-11.js
@@ -1,15 +1,23 @@
 
-//24) Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente, pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }.
-const biggerMinor=(arr=undefined)=>{
-    if(arr===undefined) return console.warn(``);
+//24) Program a function that, given an array of numbers, returns an object with two arrays, the first will have the numbers ordered in ascending order and the second in descending order, eg. myFunction ([7,5,7,8,6]) will return {asc: [5,6,7,7,8], dec: [8,7,7,6,5]}.
+const orderArray = (arr = undefined) => {
+    if (arr === undefined) return console.warn(`Please introduce a value`);
+    if (!(arr instanceof Array)) return console.error(`Please introduce an array object`);
+    if (arr.length === 0) return console.warn(`Please introduce an array`);
 
+    for (let i of arr) {
+        if (typeof i !== `number`) return console.warn(`The value ${i} is not a number`);
+    }
+
+    return console.info({
+        arr,
+        upward:arr.map(el=>el).sort(),
+        descendant:arr.map(el=>el).sort().reverse()
+
+    });
 }
 
+orderArray([7, 5,7,8,6]);
 
 
 
-/*
-25) Programa una función que dado un arreglo de elementos, elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devolverá ["x", 10, 2, "10", true].
-
-26) Programa una función que dado un arreglo de números obtenga el promedio, pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5.
-*/
