b42c55ca1c4d8b3a527eff5fa6bd3c2cae04302a
CxrlosMX <LuisCRendon131@gmail.com>
2021-11-19 09:53:56 -0600

    Exercise 25 resolved

--- 24.-Exercises/js/exercise-11.js
+++ 24.-Exercises/js/exercise-11.js
@@ -11,13 +11,40 @@ const orderArray = (arr = undefined) => {
 
     return console.info({
         arr,
-        upward:arr.map(el=>el).sort(),
-        descendant:arr.map(el=>el).sort().reverse()
+        upward: arr.map(el => el).sort(),
+        descendant: arr.map(el => el).sort().reverse()
 
     });
 }
 
-orderArray([7, 5,7,8,6]);
+//orderArray([7, 5,7,8,6]);
 
+
+
+
+//25) Program a function that, given an array of elements, eliminates duplicates, eg. myFunction (["x", 10, "x", 2, "10", 10, true, true]) will return ["x", 10, 2, "10", true].
+const removeDuplicates = (arr = undefined) => {
+    if (arr === undefined) return console.warn(`Please introduce a value`);
+    if (!(arr instanceof Array)) return console.error(`Please introduce an array object`);
+    if (arr.length === 0) return console.warn(`Please introduce an array`);
+    if (arr.length === 1) return console.warn(`The array neet to have 2 elements or more`);
+
+    // return console.info({
+    //     arr,
+    //     withoutDuplicates:arr.filter((value,index,self)=>self.indexOf(value)===index)
+
+    // });
+    return console.info({
+        arr,
+        withoutDuplicates: [... new Set(arr)]
+
+    });
+
+
+};
+removeDuplicates(["x", 10, "x", 2, "10", 10, true, true]);
+
+
+//26) Program a function that, given an array of numbers, obtains the average, eg. average ([9,8,7,6,5,4,3,2,1,0]) will return 4.5.
 
 
