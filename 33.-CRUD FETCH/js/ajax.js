4bd2315af9ffc9e100a1d6a528bb9ccb35aab601
CxrlosMX <LuisCRendon131@gmail.com>
2022-01-26 08:12:37 -0600

    CRUD AJAX-Structure HTML

---
+++ 33.-CRUD FETCH/js/ajax.js
@@ -0,0 +1,6 @@
+const d = document,
+    $table = d.querySelector(".crud-table"),
+    $form = d.querySelector(".crud-form"),
+    $title = d.querySelector(".crud-title"),
+    $template = d.getElementById("crud-template").content,
+    $fragment=d.createDocumentFragment();
