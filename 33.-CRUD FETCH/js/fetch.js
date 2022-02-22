e4c3904f3e838c636487bb19133596bee50e088e
CxrlosMX <LuisCRendon131@gmail.com>
2022-02-21 20:47:50 -0600

    CRUD-AXIOS-Api rest

--- 33.-CRUD FETCH/js/fetch.js
+++ 33.-CRUD FETCH/js/fetch.js
@@ -33,3 +33,89 @@ const getAll = async () => {
 }
 
 d.addEventListener("DOMContentLoaded", getAll);
+
+d.addEventListener("submit", async e => {
+    if (e.target === $form) {
+        e.preventDefault();
+        if (!e.target.id.value) {
+            //Create --POST
+            try {
+                let options = {
+                    method: "POST",
+                    header: {
+                        "Content-type": "application/json; charset=utf-8"
+                    },
+                    body: JSON.stringify({
+                        nombre: e.target.name.value,
+                        constelacion: e.target.power.value
+                    })
+                },
+                    res = await fetch("http://localhost:3000/santos", options),
+                    json = await res.json();
+                if (!res.ok) throw { status: res.status, statusText: res.statusText };
+                location.reload();
+            } catch (err) {
+                let message = err.statusText || "There's been a problem";
+                $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`)
+            }
+
+
+        }
+        else {
+            //Update --PUT
+            try {
+                let options = {
+                    method: "PUT",
+                    header: {
+                        "Content-type": "application/json; charset=utf-8"
+                    },
+                    body: JSON.stringify({
+                        nombre: e.target.name.value,
+                        constelacion: e.target.power.value
+                    })
+                },
+                    res = await fetch(`http://localhost:3000/santos/${e.target.dataset.id}`, options),
+                    json = await res.json();
+                if (!res.ok) throw { status: res.status, statusText: res.statusText };
+                location.reload();
+            } catch (err) {
+                let message = err.statusText || "There's been a problem";
+                $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`)
+            }
+
+        }
+    }
+});
+
+
+d.addEventListener("click",async (e) => {
+    if (e.target.matches(".edit")) {
+        alert("You pressed the button Edit");
+        $title.textContent = "Editant";
+        $form.name.value = e.target.dataset.name;
+        $form.power.value = e.target.dataset.constellation;
+        $form.id.value = e.target.dataset.id;
+
+    }
+    if (e.target.matches(".delete")) {
+        let isDelete = confirm("Are you sure you want to delete?");
+        if (isDelete) {
+            try {
+                let options = {
+                    method: "DELETE",
+                    header: {
+                        "Content-type": "application/json; charset=utf-8"
+                    }
+
+                },
+                    res = await fetch(`http://localhost:3000/santos/${e.target.dataset.id}`, options),
+                    json = await res.json();
+                if (!res.ok) throw { status: res.status, statusText: res.statusText };
+                location.reload();
+            } catch (err) {
+                let message = err.statusText || "There's been a problem";
+                $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`)
+            }
+        }
+    }
+});
--- 34.-CRUD-AXIOS/js/ajax.js
+++
@@ -1,6 +0,0 @@
-const d = document,
-    $table = d.querySelector(".crud-table"),
-    $form = d.querySelector(".crud-form"),
-    $title = d.querySelector(".crud-title"),
-    $template = d.getElementById("crud-template").content,
-    $fragment=d.createDocumentFragment();
