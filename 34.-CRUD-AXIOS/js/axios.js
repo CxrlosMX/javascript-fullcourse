8fa5cb8ab008f910319993c98eca2db1ffbf0eb7
CxrlosMX <LuisCRendon131@gmail.com>
2022-02-28 07:00:25 -0600

    CRUD-AXIOS-Api rest

--- 34.-CRUD-AXIOS/js/axios.js
+++ 34.-CRUD-AXIOS/js/axios.js
@@ -9,20 +9,20 @@ const getAll = async () => {
     try {
         let res = await axios.get("http://localhost:3000/santos"),
             json = await res.data;
-            json.forEach(el => {
-                $template.querySelector(".name").textContent = el.nombre;
-                $template.querySelector(".power".textContent = el.constelacion);
-                $template.querySelector(".edit").dataset.id = el.id;
-                $template.querySelector(".edit").dataset.name = el.nombre;
-                $template.querySelector(".edit").dataset.constellation = el.constelacion;
-                $template.querySelector(".delete").dataset.id = el.id;
-    
-                let $clone = d.importNode($template, true);
-    
-                $fragment.appendChild($clone);
-    
-            });
-            $table.querySelector("tbody").appendChild($fragment);
+        json.forEach(el => {
+            $template.querySelector(".name").textContent = el.nombre;
+            $template.querySelector(".power".textContent = el.constelacion);
+            $template.querySelector(".edit").dataset.id = el.id;
+            $template.querySelector(".edit").dataset.name = el.nombre;
+            $template.querySelector(".edit").dataset.power = el.constelacion;
+            $template.querySelector(".delete").dataset.id = el.id;
+
+            let $clone = d.importNode($template, true);
+
+            $fragment.appendChild($clone);
+
+        });
+        $table.querySelector("tbody").appendChild($fragment);
     } catch (err) {
         let message = err.statusTex || "There's been a problem";
         $table.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}:${message}</b></p>`);
@@ -31,3 +31,85 @@ const getAll = async () => {
 }
 
 d.addEventListener("DOMContentLoaded", getAll);
+d.addEventListener("submit", async e => {
+    if (e.target === $form) {
+        e.preventDefault();
+
+        if (!e.target.id.value) {
+            //Create--POST
+            try {
+                let options = {
+                    method: "POST",
+                    header: {
+                        "Content-type": "application/json; charset=utf-8"
+                    },
+                    data: JSON.stringify({
+                        nombre: e.target.name.value,
+                        constelacion: e.target.power.value
+                    })
+                },
+                    res = await axios("http://localhost:3000/santos", options),
+                    json = await res.data;
+                location.reload();
+            } catch (err) {
+                let message = err.statusText || "There's been a problem";
+                $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}:${message}</b></p>`);
+
+            }
+        }
+        else {
+            //Update--PUT
+            try {
+                let options = {
+                    method: "PUT",
+                    header: {
+                        "Content-type": "application/json; charset=utf-8"
+                    },
+                    data: JSON.stringify({
+                        nombre: e.target.name.value,
+                        constelacion: e.target.power.value
+                    })
+                },
+                    res = await axios(`http://localhost:3000/santos/${e.target.id.value}`, options),
+                    json = await res.data;
+                location.reload();
+            } catch (err) {
+                let message = err.statusText || "There's been a problem";
+                $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}:${message}</b></p>`);
+
+            }
+        }
+
+    }
+});
+
+d.addEventListener("click", async e => {
+    if (e.target.matches(".edit")) {
+        $title.textContent = "Editing Santos";
+
+        $form.name.value = e.target.dataset.name;
+        $form.power.value = e.target.dataset.constellation;
+        $form.id.value = e.target.dataset.id;
+    }
+    if (e.target.matches(".delete")) {
+        let isDelete = confirm("Are you sure you want to delete?");
+        if (isDelete) {
+            try {
+                let options = {
+                    method: "DELETE",
+                    header: {
+                        "Content-type": "application/json; charset=utf-8"
+                    }
+                    
+                },
+                    res = await axios(`http://localhost:3000/santos/${e.target.dataset.id}`, options),
+                    json = await res.data;
+                location.reload();
+            } catch (err) {
+                let message = err.statusText || "There's been a problem";
+                $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}:${message}</b></p>`);
+
+            }
+        }
+    }
+});
